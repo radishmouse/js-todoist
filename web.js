231c5f7395b01c787e3ff5808afdc307ed50c4a2
Chris Aquino <chris.aquino@gmail.com>
2018-11-02 22:30:38 -0400

    adds template functions for drawing Todo items

--- web.js
+++ web.js
@@ -1,12 +1,52 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
+const {Todo} = require('./lib');
+
+function page(content) {
+  return `
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Important Stuffs</title>   
+
+  </head>
+  <body>
+    ${content}
+  </body>
+</html>
+  `;
+}
+
+function todoToListItem(todo) {
+  let href = todo.url ? `href="${todo.url}"`: '';
+  
+  return `
+  <li>
+    <a ${href} target="_blank" rel="noopener noreferrer">
+      ${todo.title}
+    </a>
+  </li>
+  `;
+}
+
+async function todoList() {
+  let todosArray = await Todo.findAll();
+  let todoListItems = todosArray.map(todoToListItem).join('');
+
+  return `
+    <ul>
+      ${todoListItems}
+    </ul>
+  `;
+}
+
+app.get('/', async (req, res) => {
+  let todos = await todoList();
   res
     .status(200)
-    .end(`
-<h1>hey</h1>
-    `);
+    .end(page(todos));
 });
 
 module.exports = () => {
